0bf1a15b089bb1450d683ee90a4543a67d52e1ae
Chad Chapman <CChapman36820@tacomacc.edu>
2016-07-18 09:30:43 -0700

    added in js functionality
    chose to go with some jQuery instead of other routes for learning purposes, still lacks the scrollspy feature

---
+++ project2JS.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+$(".faf").css("font-color", "white");
+$(".footer").css("background-color", "purple");
+$(".semi-footer-text, .footer, #footerRtText, #contact-me-text").addClass("text-center");
+//$(".footer-icon-span").addClass("center-block");
+$("#contact-me-text").text("I changed this with jQuery");
+$("contact-me-hidden").hide();
+
+$("#fafon").tooltip({
+	trigger: "hover",
+	title: "TEXT ME"
+});
+
+});
+
+$('body').scrollspy({target: '.navbar-top'})
